6c361a0f215f9a33ade304a203a484231a57044b
Eivind Gussiås Løkseth <eivind@outlok.com>
2026-05-03 11:41:23 +0200

    Fix sql bash script

--- .devcontainer/mssql/postCreateCommand.sh
+++ .devcontainer/mssql/postCreateCommand.sh
@@ -28,29 +28,29 @@ then
     exit 1
 fi
 
-for f in $dacpath/*
+for f in "$dacpath"/*
 do
-    if [ $f == $dacpath/*".dacpac" ]
+    if [[ "$f" == "$dacpath"/*.dacpac ]]
     then
         dacpac="true"
         echo "Found dacpac $f"
     fi
 done
 
-for f in $sqlpath/*
+for f in "$sqlpath"/*
 do
-    if [ $f == $sqlpath/*".sql" ]
+    if [[ "$f" == "$sqlpath"/*.sql ]]
     then
         sqlfiles="true"
         echo "Found SQL file $f"
     fi
 done
 
-if [ $sqlfiles == "true" ]
+if [ "$sqlfiles" == "true" ]
 then
-    for f in $sqlpath/*
+    for f in "$sqlpath"/*
     do
-        if [ $f == $sqlpath/*".sql" ]
+        if [[ "$f" == "$sqlpath"/*.sql ]]
         then
             echo "Executing $f"
             "$sqlcmd_bin" -C -S localhost -U sa -P "$SApassword" -d master -i "$f"
@@ -58,11 +58,11 @@ then
     done
 fi
 
-if [ $dacpac == "true" ] 
+if [ "$dacpac" == "true" ]
 then
-    for f in $dacpath/*
+    for f in "$dacpath"/*
     do
-        if [ $f == $dacpath/*".dacpac" ]
+        if [[ "$f" == "$dacpath"/*.dacpac ]]
         then
             dbname=$(basename $f ".dacpac")
             echo "Deploying dacpac $f"
